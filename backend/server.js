da944a260b2ed1cbf3a47f89813931e88cb1d550
ampersand22 <jeyulmer@gmail.com>
2022-09-07 22:39:29 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,18 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const db = mongoose.connection;
 require('dotenv').config()
+
+//// MIDDLEWEAR
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+
+
+/// LISTENING ////
+
+app.listen(PORT, () => {
+    console.log('listening on ', PORT, ' homie...');
+  })
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+
+
+
+
+
+  
+  return (
+    <div className="App">
+    
+      <div className="container-fluid">
+
+        
+
+      </div>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
